1f71f41c3fcb8c209ca6df351fad37bde600f4c6
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-08-29 20:20:44 +0300

    clear unused links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Home from './pages/home/home';
-// import Contacts from './pages/contacts/contacts.jsx';
-// import About from './pages/about/about.jsx';
 
 function App() {
   return (
@@ -15,8 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route path="/contacts" element={<Contacts />} /> */}
-          {/* <Route path="/about" element={<About />} /> */}
         </Routes>
         <Footer />
       </div>
